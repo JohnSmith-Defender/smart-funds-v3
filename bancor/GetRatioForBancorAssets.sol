712a5757035e8a38fda0694a8ffee6f7bf00f6e1
johnsmith-defender <johnsmith.diamon@gmail.com>
2020-01-10 14:48:52 +0200

    Fixed bug in GetRatioForBancorAssets

--- bancor/GetRatioForBancorAssets.sol
+++ bancor/GetRatioForBancorAssets.sol
@@ -1,25 +1,36 @@
 pragma solidity ^0.4.24;
 
 import "./PathFinderInterface.sol";
-import "./BancorNetworkInterface";
+import "./BancorNetworkInterface.sol";
+import "../zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract GetRatioForBancorAssets {
   BancorNetworkInterface public bancorNetwork;
   PathFinderInterface public pathFinder;
 
-  constructor(address _bancorNetwork, _pathFinder) public{
+  constructor(address _bancorNetwork, address _pathFinder) public{
     bancorNetwork = BancorNetworkInterface(_bancorNetwork);
     pathFinder = PathFinderInterface(_pathFinder);
   }
 
   // Get Ration between Bancor assets
-  function getRatio(address _from, address _to, uint256 _amount) public view return(uint256 result){
+  // Get Ration between Bancor assets
+  function getRatio(address _from, address _to, uint256 _amount) public view returns(uint256 result){
     if(_amount > 0){
       // get Bancor path array
-      address[] path = pathFinder.generatePath(_from, _to);
+      address[] memory path = pathFinder.generatePath(_from, _to);
+      ERC20[] memory pathInERC20 = new ERC20[](path.length);
+
+      // Convert addresses to ERC20
+      for(uint i=0; i<path.length; i++){
+          pathInERC20[i] = ERC20(path[i]);
+      }
+      
       // get Ratio
-      result = bancorNetwork.getReturnByPath(path, _amount);
-    }else{
+      ( uint256 ratio, ) = bancorNetwork.getReturnByPath(pathInERC20, _amount);
+      result = ratio;
+    }
+    else{
       result = 0;
     }
   }
